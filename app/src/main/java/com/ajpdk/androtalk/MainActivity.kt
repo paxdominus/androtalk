f3bbe7f5cc22948f28b6bc5826671c171776a285
paxdominus <aswin.ajp@gmail.com>
2022-12-27 18:07:20 +0530

    hello

--- app/src/main/java/com/ajpdk/androtalk/MainActivity.kt
+++ app/src/main/java/com/ajpdk/androtalk/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ajpdk.androtalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.ajpdk.androtalk.databinding.ActivityMainBinding
+
+
+import com.ajpdk.androtalk.ml.GPT2Client
 
 class MainActivity : AppCompatActivity() {
+    private val gpt2: GPT2Client by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding
+                = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Bind layout with ViewModel
+        binding.vm = gpt2
+
+        // LiveData needs the lifecycle owner
+        binding.lifecycleOwner = this
     }
 }
